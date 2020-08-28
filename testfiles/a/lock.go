c07b3d9311b0ef0c6035ca031b6ac2bcb5a07b6c
yukatayu <yukatayu.dev@gmail.com>
2020-08-28 20:29:02 +0900

    fix: test: struct

--- testfiles/a/lock.go
+++ testfiles/a/lock.go
@@ -27,6 +27,6 @@ type Lock struct {
 	ID    int64  `dynamo:"id,hash"     validate:"required"`
 	Name  string `dynamo:"name,unique"`
 	Name2 string `dynamo:"name2,unique"`
-	Email string `dynamo:"email"       validate:"email"`
+	Email string `dynamo:"email"`
 	Meta  Nest1Type
 }
